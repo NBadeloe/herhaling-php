bd03fd88e6bd5dbc5c9f891efc772bdaacfa6440
NBadeloe <48018083+NBadeloe@users.noreply.github.com>
2020-09-02 23:57:25 +0200

    Add files via upload
    hw sep 1

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+// variables
+    // $getal = 1;
+    // var_dump(is_int($getal));
+
+//array+forl loop
+// $klas = ["Piet van Dijk", "Rembrandt van Rijn", "Vincent van Gogh", "Floris van Dyck"];
+// var_dump($klas);
+
+// for ($i=0; $i <=3; $i++) { 
+//     print $klas[$i] . "<br>";
+// }
+
+    // foreach ($klas as $leerling ) {
+    //     print $leerling . "<br>";
+    // }
+
+//combinatie opdracht
+    // $bucket_list[];
+    // $hoeveelheid_activiteiten =  readline("hoeveel activiteiten wil je toevoegen aan je bucketlist?");
+    
+
+    // $count = $hoeveelheid_activiteiten;
+    //     for ($i=0; $i <$count ; $i++) { 
+    //             $activity = readline("welke activiteiten wil je toevoegen?") . "<br>";
+    //             readline_add_history($activity); 
+                  
+    //         }
+    //         array_push($bucket_list, $activity);
+    //         var_dump($activity);
+        
+    //     for ($i=0; $i <count($activity) ; $i++) { 
+    //         echo $activity. "<br>";
+    //     }
+?>
